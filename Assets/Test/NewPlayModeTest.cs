5a4d348715e6f53701306adcf6581f38f1ac1d23
kkkon <kiyofumi.kondoh+github@gmail.com>
2018-07-29 18:04:24 +0900

    add directive guard.   ENABLE_PLAYMODE_TESTS_RUNNER always true   after Unity 5.6

--- Assets/Test/NewPlayModeTest.cs
+++ Assets/Test/NewPlayModeTest.cs
@@ -1,4 +1,39 @@
-﻿using UnityEngine;
+﻿/*
+ * The MIT License
+ *
+ * Copyright 2018 kkkon.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#if false
+#if UNITY_5_6 || UNITY_2017_1 || UNITY_2017_2
+// https://issuetracker.unity3d.com/issues/enable-playmode-tests-runner-define-is-true-even-when-playmode-tests-are-disabled
+//  Assembly definition
+#define USE_PLAYMODE_TESTS_RUNNER
+#endif
+#endif
+
+
+#if ENABLE_PLAYMODE_TESTS_RUNNER && USE_PLAYMODE_TESTS_RUNNER
+
+using UnityEngine;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
@@ -19,3 +54,5 @@ public class NewPlayModeTest {
 		yield return null;
 	}
 }
+
+#endif // ENABLE_PLAYMODE_TESTS_RUNNER && USE_PLAYMODE_TESTS_RUNNER
